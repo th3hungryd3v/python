c48cd543771263465a524c3252ded801ad044d76
th3hungryd3v <th3hungryd3v@gmail.com>
2021-08-13 09:42:58 -0700

    Task 4 battlegame.py

--- 1-Fundamentals/battlegame.py
+++ 1-Fundamentals/battlegame.py
@@ -1,3 +1,5 @@
+# Global Character and Stat Variables
+# Task 1 - Set up your game variables: the game characters and their stats.
 wizard = "Wizard"
 elf = "Elf"
 human = "Human"
@@ -10,22 +12,43 @@ human_damage = 20
 dragon_hp = 300
 dragon_damage = 50
 
+# Task 2 + Task 3 - Prompt the player to choose from a list of options. + set up infinite loop to handle player choice.
 while True:
     print("1)   Wizard")
     print("2)   Elf")
     print("3)   Human")
-    character = input("Choose your character: ")
-    if character == 1 or "wizard":
+    character = int(input("Choose your character: "))
+    if character == 1:
+        character = wizard
         my_hp = wizard_hp
         my_damage = wizard_damage
+        print("You have chosen the character: " + character)
+        print("Health: " + str(my_hp))
+        print("Damage: " + str(my_damage))
         break
-    if character == 2 or "elf":
+    elif character == 2:
+        character = elf
         my_hp = elf_hp
         my_damage = elf_damage
+        print("You have chosen the character: " + character)
+        print("Health: " + str(my_hp))
+        print("Damage: " + str(my_damage))
         break
-    if character == 3 or "human":
+    elif character == 3:
+        character = human
         my_hp = human_hp
         my_damage = human_damage
+        print("You have chosen the character: " + character)
+        print("Health: " + str(my_hp))
+        print("Damage: " + str(my_damage))
         break
     else:
         print("Unknown character")
+# Task 4 - Battle with the Dragon!
+while True:
+    dragon_hp = dragon_hp - my_damage
+    print("The " + character + " damaged the Dragon!")
+    print("The Dragon's hitpoints are now: " + str(dragon_hp))
+    if dragon_hp == 0:
+        print("The Dragon has lost the battle")
+        break
