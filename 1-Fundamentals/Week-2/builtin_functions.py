81d33cf7f2af7dbd7a3da20bbbcc4c507f8b108a
th3hungryd3v <th3hungryd3v@gmail.com>
2021-08-16 19:25:47 -0700

    builtin_functions.py lesson halfway done

---
+++ 1-Fundamentals/Week-2/builtin_functions.py
@@ -0,0 +1,16 @@
+print("Math Functions")
+
+# The abs() function takes a numeric (integer or float) argument and returns its absolute value. 
+abs_int = abs(-1)
+print(abs_int)
+
+# The float() function takes a numeric argument, converts it to a floating point value, and returns it.
+int_to_float = float(100)
+print(int_to_float)
+
+# The int() function takes a numeric argument, converts it to an integer value, and returns it
+float_to_int = int(1.23)
+print(float_to_int)
+
+
+
