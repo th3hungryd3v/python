20cb1a2e117c28c3e8cdcb282e9e098e665e49ea
th3hungryd3v <th3hungryd3v@gmail.com>
2021-08-17 13:40:01 -0700

    custom_functions.py

---
+++ 1-Fundamentals/Week-2/custom_functions.py
@@ -0,0 +1,12 @@
+# Function Name:
+# Cannot be a reserved keyword (while, for, etc)
+# No Spaces
+# Must start with a letter or an underscore
+"""
+Function Syntax:
+
+def function_name():
+    statement
+    statement
+    etc..,
+"""
