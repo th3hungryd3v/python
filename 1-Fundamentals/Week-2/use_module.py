ee3f1250c9ec89364d6bfa41bfa5725cf98441bf
th3hungryd3v <th3hungryd3v@gmail.com>
2021-08-18 12:20:45 -0700

    creating and using modules

---
+++ 1-Fundamentals/Week-2/use_module.py
@@ -0,0 +1,6 @@
+# import my_module
+# can also be done like this
+from my_module import greet, flavor
+
+greet("Albert Einstein") # using from, you can import only certain variables that is needed, now this isn't needed -- my_module. 
+print("My favorite ice cream flavor is", flavor) # or my_module. 
